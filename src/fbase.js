5d2c30e17ab77e462dad9ca2e8ec392e47d345cc
hoolabet <95078865+hoolabet@users.noreply.github.com>
2022-03-28 12:03:49 +0900

    getNweets

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import firebase from "firebase/compat/app";
 import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -15,3 +16,4 @@ const firebaseConfig = {
 
   export const firebaseInstance = firebase;
   export const authService = getAuth(app);
+  export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,74 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+import { addDoc, getDocs, collection ,query , orderBy, onSnapshot } from "firebase/firestore";
 
-const Home = () => <span>Home</span>;
-
+const Home = ({userObj}) => {
+    const [nweet,setNweet] = useState("");
+    const [nweets,setNweets] = useState([]);
+    const getNweets = async() => {
+        const q = query(collection(dbService,"nweets"));
+           const querySnapshot = await getDocs(q);
+           querySnapshot.forEach((doc) => {
+               const nweetObj = {
+                   ...doc.data(),
+                   id:doc.id,
+               }
+               setNweets((prev) => [nweetObj,...prev]);
+           });
+    };
+    useEffect(() => {
+        getNweets();
+        const q = query(collection(dbService,"nweets"),
+            orderBy("createdAt","desc"));
+        onSnapshot(q,(snapshot) => {
+            const nweetArr = snapshot.docs.map((doc) => ({
+                id:doc.id,
+                ...doc.data(),
+            }));
+            setNweets(nweetArr);
+        });
+    },[]);
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        try {
+            const docRef = await addDoc(collection(dbService,"nweets"),{
+            text: nweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+        });
+        console.log("Document written with ID:",docRef.id);
+        } catch(error){
+            console.error("Error adding document:",error);
+        }
+        setNweet("");
+    };
+    const onChange = (event) => {
+        const { target:{value},} = event;
+        setNweet(value);
+    };
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input 
+                value={nweet}
+                onChange={onChange}
+                type="text" 
+                placeholder="What's on your mind?"
+                maxLength={120}
+            />
+            <input 
+                type="submit"
+                value="Nweet"
+            />
+        </form>
+        <div>
+            {nweets.map(nweet => (
+                <div key={nweet.id}>
+                    <h4>{nweet.text}</h4>
+                </div>
+            ))}
+        </div>
+    </div>
+);
+};
 export default Home;
